0317b08dee85d6136f97f8fafce5893264f90eb0
Constantin <jaecktec@noreply.github.com>
2020-07-19 19:22:21 +0200

    🔥 remove unused code

--- webapp/src/components/message-log.tsx
+++ webapp/src/components/message-log.tsx
@@ -1,12 +1,10 @@
-import React, {RefObject, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './message-log.css'
 import {useWebsocket} from "../websocket/web-socket-context";
 
 export const MessageLog: React.FC<any> = (): React.ReactElement => {
     const {recentMessage} = useWebsocket()
     const [messages, setMessages] = useState<string[]>([])
-    const listRootRef = React.createRef();
-    let bottomElem: RefObject<any>;
 
     useEffect(() => {
         setMessages((messages) => {
@@ -14,13 +12,11 @@ export const MessageLog: React.FC<any> = (): React.ReactElement => {
             newVar.length = Math.min(15, newVar.length);
             return newVar;
         })
-        if(bottomElem && bottomElem.current) bottomElem.current.scrollIntoView();
     }, [recentMessage])
 
     return (<div className="message-log">
         {
             messages.map((value, index) => {
-                    bottomElem = React.createRef();
                     return (
 
                         <p key={`message_${index}`}>{value}</p>
